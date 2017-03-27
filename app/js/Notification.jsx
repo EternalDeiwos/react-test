aae78ed38f5ba9f8386478fc4aecb37a728248f0
Greg Linklater <greglink49@gmail.com>
2017-03-27 21:56:44 +0200

    refactor(Notification): change notification color

--- app/js/Notification.jsx
+++ app/js/Notification.jsx
@@ -2,6 +2,13 @@
 
 import React from 'react'
 import Snackbar from 'material-ui/Snackbar'
+import { grey700 } from 'material-ui/styles/colors'
+
+const style = {
+  snackbarStyle: {
+    backgroundColor: grey700
+  }
+}
 
 class Notification extends React.Component {
 
@@ -15,12 +22,15 @@ class Notification extends React.Component {
   }
 
   render () {
+    let { snackbarStyle } = style
+
     return (
       <Snackbar
         open={this.state.open}
         message={this.props.message}
         autoHideDuration={4000}
         onRequestClose={() => this.handleClose()}
+        bodyStyle={snackbarStyle}
       />
     )
   }
